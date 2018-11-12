9ed9d7b0e7aaa1ba057ca7ae9419cd3d598b7203
Tonia Tkachuk <tonia.tkachuk@gmail.com>
2018-11-12 11:39:32 +0800

    Scroll to sections on keys up, down

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,65 @@
 $(document).ready(
   function() {
+    // custom variablees, functions
+    var scrollTime = 1000;
+
+    function scrollToTop(id) {
+      $([document.documentElement, document.body]).animate({
+              scrollTop: $(id).offset().top
+          }, scrollTime);
+       //console.log("on " + id);
+    }
+
     // menu functionality
-     $(".burger-button").click(function(){
+     $(".burger-button").click(function() {
        $(".burger-button").toggleClass("active");
        $(".burger-menu").slideToggle();
      });
 
-     $(".burger-menu").click(function(){
+     $(".burger-menu").click(function() {
        $(".burger-button").toggleClass("active");
        $(".burger-menu").slideToggle();
      });
+
+     // scroll to sections on up and down buttons
+     $(document).keydown(function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 40) {
+          // "down"
+          if ($("#publications").visible(true)) {
+            scrollToTop("#contact-me");
+
+          } else if ($("#talks_and_workshops").visible(true)) {
+            scrollToTop("#publications");
+
+          } else if ($("#technology").visible(true)) {
+            scrollToTop("#talks_and_workshops");
+
+          } else if ($("#projects").visible(true)) {
+            scrollToTop("#technology");
+
+          } else if ($("#about-me").visible(true)) {
+            scrollToTop("#projects");
+          }
+        } else if (code == 38) {
+          // "up"
+          if ($("#projects").visible(true)) {
+            scrollToTop("#about-me");
+
+          } else if ($("#technology").visible(true)) {
+            scrollToTop("#projects");
+
+          } else if ($("#talks_and_workshops").visible(true)) {
+            scrollToTop("#technology");
+
+          } else if ($("#publications").visible(true)) {
+            scrollToTop("#talks_and_workshops");
+
+          } else if ($("#contact-me").visible(true)) {
+            scrollToTop("#publications");
+          }
+        }
+    });
 
     // SMOOTH SCROLL JQUERY CODE
     // Select all links with hashes
@@ -33,7 +83,7 @@ $(document).ready(
             event.preventDefault();
             $('html, body').animate({
               scrollTop: target.offset().top
-            }, 1000, function() {
+            }, scrollTime, function() {
               // Callback after animation
               // Must change focus!
               var $target = $(target);
@@ -49,6 +99,5 @@ $(document).ready(
         }
       });
     // END SMOOTH SCROLL JQUERY CODE
-
   }
 );
